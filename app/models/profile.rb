a98b84e85fa9c5603af7c6be0dd3109f947eb7aa
kasei_san <kasei.san@gmail.com>
2016-06-26 00:23:32 +0900

    ポリモーフィック下準備: 更新時にprofilableを更新するように変更

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,12 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  before_save :set_profilable_id
+
+  def set_profilable_id
+    if self.has_attribute?(:profilable_id)
+      self.profilable_id   = self.user_id
+      self.profilable_type = 'User'
+    end
+  end
 end
