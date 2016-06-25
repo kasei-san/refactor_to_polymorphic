71a46ecca55050bfb9ca666ea5c58d114a1afb42
kasei_san <kasei.san@gmail.com>
2016-06-26 01:36:49 +0900

    ポリモーフィック関連に移行

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,12 +1,3 @@
 class Profile < ActiveRecord::Base
-  belongs_to :user
-
-  before_save :set_profilable_id
-
-  def set_profilable_id
-    if self.has_attribute?(:profilable_id)
-      self.profilable_id   = self.user_id
-      self.profilable_type = 'User'
-    end
-  end
+  belongs_to :profilable, polymorphic: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  has_one :profile, dependent: :destroy
+  has_one :profile, :as => :profilable
   accepts_nested_attributes_for :profile
 end
